1ffb1cc75dc234b064940aaadd4eac4b398d38cc
KomodorDev <sah_services@outlook.com>
2025-10-25 19:18:01 +0800

    Formatting

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 # main.py
 from controllers.app_controller import AppController
 
+# ----------------------------------------------------------------
 def main():
     """Entry point for the Gradio application."""
     app = AppController()
     app.launch()
+
+# ----------------------------------------------------------------
 
 if __name__ == "__main__":
     main()
