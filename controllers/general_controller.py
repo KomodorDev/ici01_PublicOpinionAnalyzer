bdc40915b57a01deb3f197fbf7afe63a81aa040a
KomodorDev <sah_services@outlook.com>
2025-10-25 17:00:38 +0800

    Formatting

--- controllers/general_controller.py
+++ controllers/general_controller.py
@@ -3,14 +3,17 @@
 from views.general_view import GeneralView
 from services.general_service import GeneralService
 
+##################################################################
 class GeneralController:
     """Handles logic and coordination for the General tab."""
 
+    # ----------------------------------------------------------------
     def __init__(self):
         # Explicit and self-documenting instance names
         self.general_service = GeneralService()
         self.general_view = GeneralView()
 
+    # ----------------------------------------------------------------
     def render_general_view(self):
         """Get data from service, pass it to view for display."""
         # Ask service for data then give it to view:
@@ -18,3 +21,6 @@ class GeneralController:
         # Available Label Groups
 
         self.general_view.render_general_view()
+    # ----------------------------------------------------------------
+
+##################################################################
