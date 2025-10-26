aa5488380f43a55b28c300cfbd63d6a6a07ad5b6
KomodorDev <sah_services@outlook.com>
2025-10-26 12:10:07 +0800

    Added SettingsView to package

--- views/__init__.py
+++ views/__init__.py
@@ -7,6 +7,7 @@ Holds all view classes responsible for app coordination.
 
 from .general_view import GeneralView
 from .classification_view import ClassificationView
+from .settings_view import SettingsView
 
 # Define what’s publicly importable from this package
-__all__ = ["GeneralView", "ClassificationView"]
+__all__ = ["GeneralView", "ClassificationView", "SettingsView"]
