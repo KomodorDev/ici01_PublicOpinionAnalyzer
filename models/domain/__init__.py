b6f9af1d7e6270c145af4e6e35fe842c4380271f
KomodorDev <sah_services@outlook.com>
2025-11-16 13:58:03 +0800

    Updated model init

--- models/domain/__init__.py
+++ models/domain/__init__.py
@@ -13,6 +13,7 @@ from .comment_model import Comment
 from .classification_model import Classification
 from .classification_group_model import ClassificationGroup
 from .video_model_info_model import VideoModelInfo
+from .model_run_progress_model import ModelRunProgress
 
 # Define what’s publicly importable from this package
-__all__ = ["Classification", "ClassificationGroup", "ContentAnalysis", "ContentItem", "Comment", "LLMModelInfo", "PromptTemplate", "Label", "VideoModelInfo"]
+__all__ = ["Classification", "ClassificationGroup", "ContentAnalysis", "ContentItem", "Comment", "LLMModelInfo", "PromptTemplate", "Label", "VideoModelInfo", "ModelRunProgress"]
