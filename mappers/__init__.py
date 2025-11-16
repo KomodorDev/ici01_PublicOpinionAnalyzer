b7e008c2237ddfb45a464c69c561f791e324a771
KomodorDev <sah_services@outlook.com>
2025-11-16 13:58:30 +0800

    Outsourced all analysis controller mapper functions to dedicated analysisMapper

---
+++ mappers/__init__.py
@@ -0,0 +1,10 @@
+"""
+mappers package
+-------------------
+Holds all model classes used throughout the application.
+"""
+
+from .analysis_mapper import AnalysisMapper
+
+# Define what’s publicly importable from this package
+__all__ = ["AnalysisMapper"]
