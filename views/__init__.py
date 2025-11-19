0a77f65610e7b736aa31bac6255e8f7f686f20f5
KomodorDev <sah_services@outlook.com>
2025-11-19 20:27:28 +0800

    Removed GeneralView

--- views/__init__.py
+++ views/__init__.py
@@ -5,11 +5,10 @@ Holds all view classes responsible for app coordination.
 
 """
 
-from .general_view import GeneralView
 from .analysis_view import AnalysisView
 from .classification_view import ClassificationView
 from .settings_view import SettingsView
 from .prompt_template_view import PromptTemplateView
 
 # Define what’s publicly importable from this package
-__all__ = ["GeneralView", "AnalysisView", "ClassificationView", "SettingsView", "PromptTemplateView"]
+__all__ = ["AnalysisView", "ClassificationView", "SettingsView", "PromptTemplateView"]
