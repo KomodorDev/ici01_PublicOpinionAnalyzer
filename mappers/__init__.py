5468171250a80afa0ef477290cf5122415f0ed59
KomodorDev <sah_services@outlook.com>
2025-11-23 04:14:03 +0800

    ClassificationMapper added

--- mappers/__init__.py
+++ mappers/__init__.py
@@ -5,6 +5,7 @@ Holds all model classes used throughout the application.
 """
 
 from .analysis_mapper import AnalysisMapper
+from .classification_mapper import ClassificationMapper
 
 # Define what’s publicly importable from this package
-__all__ = ["AnalysisMapper"]
+__all__ = ["AnalysisMapper", "ClassificationMapper"]
